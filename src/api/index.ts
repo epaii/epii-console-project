8a461ac4aeebc636db3c5cff5dfae48ab911ef0f
suyahu <751198764@qq.com>
2023-09-22 08:37:02 +0800

    yh

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,15 +4,20 @@ import Log from "./../logs";
 const html = fs.readFileSync(__dirname + "/../web/index.html").toString();
 
 export default defineController({
-    index(ctx) {
-        ctx.html(fs.readFileSync(__dirname + "/../web/index.html").toString());
-    },
-    receive(ctx) {
-        let lstId = ctx.params("lastId", 0);
-        let list = Log.getLogs(lstId);
-        return {
-            logs: list,
-            lastId:list.length>0?list[0].id:lstId,
-        }
-    }
+  index(ctx) {
+    ctx.html(fs.readFileSync(__dirname + "/../web/index.html").toString());
+  },
+  receive(ctx) {
+    let lstId = ctx.params("lastId", 0);
+    let list = Log.getLogs(lstId);
+    return {
+      logs: list,
+      lastId: list.length > 0 ? list[0].id : lstId,
+    };
+  },
+  clear() {
+    Log.logs = [];
+    Log.lastId = 0
+    return 1;
+  },
 });
